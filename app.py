9659d1498122557b14d537e04f2b0f1aa5849f7f
Frederik3152 <frederik.toernstroem@gmail.com>
2023-11-30 14:56:04 +0100

    Added function to insert pictures into database

--- app.py
+++ app.py
@@ -4,20 +4,9 @@ import psycopg2
 from PIL import Image
 from io import BytesIO
 from datetime import date
-import configparser
+from config import config
 
 app = Flask(__name__, static_folder='static')
-
-# Function to parse the database configuration file
-def config(filename = 'config.ini', section = 'postgres-database'):
-    config = configparser.ConfigParser()
-    config.read(filename)
-    db = {}
-    if config.has_section(section):
-        params = config.items(section)
-        for param in params:
-            db[param[0]] = param[1]
-    return db
 
 # Set the global variable db_config as the database login credentials
 db_config = config()
---
+++ config.py
@@ -0,0 +1,12 @@
+import configparser
+
+# Function to parse the database configuration file
+def config(filename = 'config.ini', section = 'postgres-database'):
+    config = configparser.ConfigParser()
+    config.read(filename)
+    db = {}
+    if config.has_section(section):
+        params = config.items(section)
+        for param in params:
+            db[param[0]] = param[1]
+    return db
